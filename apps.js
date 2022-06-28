933330fcea51facd4ddd60aee8b002816d48cd39
Giorgadze30 <104830094+Giorgadze30@users.noreply.github.com>
2022-06-28 17:11:02 +0400

    add index.html

---
+++ apps.js
@@ -0,0 +1,15 @@
+let firstArr = [1,2,3,4,5];
+let numAdd = 1 + 2 + 3 + 4 + 5;
+let numAver = numAdd / firstArr.length;
+
+const myName = "Saba",
+	myHobby = "Drifting",
+	yearOfBirth = 2001,
+	currentYear = 2022;
+    
+    let aboutMe= {
+        firstName: "Saba",
+        lastName: "Giorgadze",
+        age: 21,
+        address: ["Temka,Tbilisi"],
+    }
